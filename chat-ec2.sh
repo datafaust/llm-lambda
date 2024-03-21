95aa4fc41713f43d49a104f5aa0180786dc5fb22
Fausto Lopez <faustolopez110@gmail.com>
2024-03-21 16:59:17 -0400

    Update chat-ec2.sh

--- chat-ec2.sh
+++ chat-ec2.sh
@@ -3,7 +3,9 @@
 sudo apt-get update -y
 
 # Install Node.js and npm
-sudo apt-get install -y nodejs npm
+curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+sudo apt-get install -y nodejs
+sudo npm install -g npm@latest -y
 
 # Install Svelte Kit globally
 #npm install -g svelte-kit
@@ -11,8 +13,23 @@ sudo apt-get install -y nodejs npm
 # Install Git
 sudo apt-get install -y git
 
-# Install Docker
-sudo apt-get install -y docker.io
+
+# docker installation
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 # Install Docker Compose
 sudo apt-get install -y docker-compose
@@ -23,9 +40,26 @@ sudo systemctl start docker
 # Enable Docker service to start on boot
 sudo systemctl enable docker
 
-# clone the repo
+# clone llm lambda repo
+git clone https://github.com/datafaust/llm-lambda.git
+
+# clone chat repo
 git clone https://github.com/huggingface/chat-ui.git
+
+# copy over docker compose file
+cp llm-lambda/docker-compose.yaml chat-ui/
 cd chat-ui
+
+# remove existing env file
+rm -f ./.env
+
+# replace below manual for now
+# create new env file
+sudo nano .env
+
+sudo npm install
+
+sudo docker-compose up -d
 
 # provide you user with docker access
 
